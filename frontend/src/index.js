6617bb10898dc1c60fd1852dd62b4f2b6d37ee36
Ororo1 <125127542+Ororo1@users.noreply.github.com>
2025-08-08 11:51:47 +0100

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,9 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
+import { createRoot } from "react-dom/client";
+import { HashRouter } from "react-router-dom";
 import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
+createRoot(document.getElementById("root")).render(
+  <HashRouter>
     <App />
-  </React.StrictMode>,
+  </HashRouter>
 );
